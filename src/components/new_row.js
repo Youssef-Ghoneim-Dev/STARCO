e01ae7dc25d99d004ec192548596b606e195e451
Youssef-Ghoneim-Dev <youssefghoneim2311@gmail.com>
2025-10-17 11:55:38 +0300

    STARCO V2

--- src/components/new_row.js
+++ src/components/new_row.js
@@ -2,7 +2,7 @@ import Button from "./button";
 export default function NewRow() {
     return(
         <div className="centered-container">
-            <div className="new-row">
+            <div className="flex_row_pdf">
                 <Button text="New Panal" class_pram="btn_Pdf" />
                 <Button text="New Page" class_pram="btn_Pdf" />
             </div>
