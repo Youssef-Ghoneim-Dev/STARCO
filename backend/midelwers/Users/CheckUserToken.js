b6824ddf4599189f449b5ccfc8036b7e4d485dfb
Youssef Ghoneim <youssefghoneim2311@gmail.com>
2026-08-06 21:02:36 +0300

    fix

--- backend/midelwers/Users/CheckUserToken.js
+++ backend/midelwers/Users/CheckUserToken.js
@@ -1,11 +1,42 @@
-module.exports = (req, res, next) => {
-    const decodedToken = req.decodedToken
-    if (decodedToken.id) {
-        next()
-    }else{
-        return res.status(403).json({
-            status: "error",
-            message: "user ID is required in Token",
-        })
+const models = require("../../models/users");
+
+module.exports = async (req, res, next) => {
+    try {
+        const decodedToken = req.decodedToken;
+
+        if (!decodedToken || !decodedToken.id) {
+            return res.status(403).json({
+                status: "error",
+                message: "user ID is required in Token",
+            });
+        }
+
+        const currentUser = await models.select_one({ _id: decodedToken.id });
+
+        if (currentUser === null) {
+            return res.status(404).json({
+                status: "error",
+                message: "User not found",
+            });
+        }
+
+        if (currentUser.isDeleted) {
+            return res.status(403).json({
+                status: "error",
+                message: "Your account has been deleted",
+            });
+        }
+
+        if (!currentUser.approved) {
+            return res.status(403).json({
+                status: "error",
+                message: "Waiting for manager approval",
+            });
+        }
+
+        req.user = currentUser;
+        next();
+    } catch (error) {
+        next(error);
     }
-}
+};
--- backend/routers/clients.js
+++ backend/routers/clients.js
@@ -1,7 +1,8 @@
 const clientsrouter = require("express").Router();
 const clientController = require("../controllers/clients")
 const authMw = require("../midelwers/auth");
+const CheckuserToken = require("../midelwers/Users/CheckUserToken");
 
-clientsrouter.get("/search", authMw, clientController.search);
+clientsrouter.get("/search", authMw, CheckuserToken, clientController.search);
 
 module.exports = clientsrouter
