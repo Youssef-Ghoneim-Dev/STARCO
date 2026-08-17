fd5ac641b84c00697f2be51a4e3f286bb0a34d56
Youssef Ghoneim <youssefghoneim2311@gmail.com>
2026-08-17 15:13:30 +0300

    fix

--- backend/utils/defaultProject.js
+++ backend/utils/defaultProject.js
@@ -10,8 +10,8 @@ const defaultProject = () => {
 
         status: "pending",
         prices: {
-            sheetPrice: 0,
-            paintPrice: 0,
+            sheetPrice: null,
+            paintPrice: null,
         },
         panels: [
             {
@@ -49,17 +49,17 @@ const defaultProject = () => {
                 ],
 
                 prices: {
-                    manufacturing: 0,
-                    locks: 0,
-                    hinges: 0,
-                    transport: 0,
-                    screws: 0,
-                    stretch: 0,
-                    copper: 0,
-                    fiber: 0,
-                    rakam: 0,
-                    fuse: 0,
-                    additionalPrice: 0
+                    manufacturing: null,
+                    locks: null,
+                    hinges: null,
+                    transport: null,
+                    screws: null,
+                    stretch: null,
+                    copper: null,
+                    fiber: null,
+                    rakam: null,
+                    fuse: null,
+                    additionalPrice: null
                 },
 
                 thickness: []
